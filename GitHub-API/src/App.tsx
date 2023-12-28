8161585caac91a013e273730836d1f80c7a4612d
Pedroo <pedrohenriquemoalive@gmail.com>
2023-12-28 20:38:42 -0300

    Integrando sistema de error

--- GitHub-API/src/App.tsx
+++ GitHub-API/src/App.tsx
@@ -3,15 +3,24 @@ import classes from "./App.module.css"
 import { UserProps } from './types/user.ts'
 import { useState } from 'react'
 import User from './components/User.tsx'
+import Error from './components/Error.tsx'
 
 const App = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
 
     const res = await fetch(`https://api.github.com/users/${userName}`)
 
     const data =  await res.json()
+
+    if (res.status === 404){
+      setError(true)
+      return
+    }
 
     const { avatar_url, login, location, followers, following} = data
 
@@ -31,6 +40,7 @@ const App = () => {
       <h1>GitHub Finder</h1>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error/>}
     </div>
 
   )
---
+++ GitHub-API/src/components/Error.tsx
@@ -0,0 +1,10 @@
+
+function Error() {
+  return (
+    <div>
+        <p>Usúario não encontrado!!</p>
+    </div>
+  )
+}
+
+export default Error
