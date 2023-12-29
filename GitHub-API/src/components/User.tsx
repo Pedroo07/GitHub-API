a64fc5a785cd6e456b872108daa5939b536d1387
Pedroo <pedrohenriquemoalive@gmail.com>
2023-12-29 08:42:19 -0300

    corrigindo erros de estilos

--- GitHub-API/src/components/User.tsx
+++ GitHub-API/src/components/User.tsx
@@ -1,25 +1,27 @@
 import { UserProps } from '../types/user.ts'
-import {MdLocationPin} from 'react-icons/md'
+import { MdLocationPin } from 'react-icons/md'
 import classes from "./User.module.css"
 
-const User = ({avatar_url,location,login,following,followers}: UserProps) => {
+const User = ({ avatar_url, location, login, following, followers }: UserProps) => {
   return (
     <div className={classes.user}>
-        <img src={avatar_url} alt={login} />
-        <h2>{login}</h2>
-        {location && <p className={classes.location}>
-            <MdLocationPin/>
-            <span>{location}</span>
-        </p>}
-        
-        <div className={classes.stats}>
-            <p>Seguidores:</p>
-            <p className={classes.number}>{followers}</p>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && <p className={classes.location}>
+        <MdLocationPin />
+        <span>{location}</span>
+      </p>}
+
+      <div className={classes.stats}>
+        <div>
+          <p>Seguidores:</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
-            <p>Seguindo:</p>
-            <p className={classes.number}>{following}</p>
+          <p>Seguindo:</p>
+          <p className={classes.number}>{following}</p>
         </div>
+      </div>
     </div>
   )
 }
